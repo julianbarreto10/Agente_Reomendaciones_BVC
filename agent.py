c06f2f3fe71fb910046800fbc348814da3bb4c81
Julian Barreto <julian.barreto.10z@gmail.com>
2025-01-15 22:56:22 -0500

    Nuevos documentos

--- agent.py
+++ agent.py
@@ -1,52 +1,68 @@
-from langchain_community.document_loaders import DirectoryLoader
 from langchain import hub
-from langchain_chroma import Chroma
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_core.prompts import PromptTemplate
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.vectorstores import FAISS
 import getpass
 import os
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+
+import nltk
+#nltk.download('punkt_tab')
+#nltk.download('averaged_perceptron_tagger_eng')
 
 os.environ["OPENAI_API_KEY"] = getpass.getpass()
 
-
-llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+stock='ecopetrol'
+llm = ChatOpenAI(model="gpt-4o-mini")
 
 loader = DirectoryLoader("agent_utils/docs_rag", glob="**/*.txt")
 docs = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 splits = text_splitter.split_documents(docs)
-vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+vectorstore = FAISS.from_documents(splits, OpenAIEmbeddings())
 
 # Retrieve and generate using the relevant snippets of the blog.
-retriever = vectorstore.as_retriever()
+retriever = vectorstore.as_retriever(search_kwargs={"k": 10})
 
-with open('template.txt', 'r') as archivo:
+
+with open('templates/template.txt', 'r') as archivo:
     # Lee el contenido del archivo y lo guarda en una variable str
     review_template_str = archivo.read()
 
 
-review_template = PromptTemplate.from_template(review_template_str)
+url_context = f'templates/{stock}_context/context.txt'
+
+with open(url_context, 'r') as archivo:
+    # Lee el contenido del archivo y lo guarda en una variable str
+    context = archivo.read()
+
+
+with open('scraping_stocks/info_accion.txt', 'r') as archivo:
+    # Lee el contenido del archivo y lo guarda en una variable str
+    valor_accion = archivo.read()
+
+review_template_str = review_template_str.replace("{valor_accion}", valor_accion).replace("{stock}", stock)
+
+custom_rag_prompt = PromptTemplate.from_template(review_template_str)
 
 
 def format_docs(docs):
+    print(len(docs))
+    print("\n\n".join(doc.page_content for doc in docs))
     return "\n\n".join(doc.page_content for doc in docs)
 
-template = review_template
-
-
-custom_rag_prompt = PromptTemplate.from_template(template)
 
 rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    {"context": retriever | format_docs, "context_stock": RunnablePassthrough()}
     | custom_rag_prompt
     | llm
     | StrOutputParser()
 )
 
-rag_chain.invoke("Ten presente el analisis de mercado y las minutas del banco de la republica que se te dieron")
+rag_chain.invoke(context)
